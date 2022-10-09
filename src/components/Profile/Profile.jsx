710e130f54b7be688d30f1a16f8a2dd7af087492
Vetali1985 <geelycross1985@gmail.com>
2022-10-09 18:25:42 +0300

    fix

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,23 +6,23 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
     <div className={css.profile}>
       <div className={css.description}>
         <img src={avatar} alt={username} className={css.avatar} />
-        <p className="name">{username}</p>
+        <p className={css.name}>{username}</p>
         <p className="tag">@{tag}</p>
         <p className="location">{location}</p>
       </div>
 
-      <ul className="stats">
-        <li>
+      <ul className={css.stats}>
+        <li className={css.statsItem}>
           <span className="label">Followers</span>
-          <span className="quantity">{stats.followers}</span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.statsItem}>
           <span className="label">Views</span>
-          <span className="quantity">{stats.views}</span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
-        <li>
+        <li className={css.statsItem}>
           <span className="label">Likes</span>
-          <span className="quantity">{stats.likes}</span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
